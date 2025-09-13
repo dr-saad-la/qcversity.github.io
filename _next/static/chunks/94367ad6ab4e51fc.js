adcc1e50891187a4583b353fc280fbd1937ac53e
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2025-09-13 23:42:23 +0100

    Deploy QCVersity: Sat Sep 13 23:42:23 CET 2025

---
+++ _next/static/chunks/94367ad6ab4e51fc.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/ec331e65af5cd6a9.js",
+  "static/chunks/turbopack-efb38b02d7d12179.js"
+])
---
+++ _next/static/chunks/fec6b622b9118a32.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/ec331e65af5cd6a9.js",
+  "static/chunks/turbopack-8bbe53afc1b44a5a.js"
+])
